9c25505c10477c91d996796e44fd3bfc67f38daa
SK MINSAR <59179357+skminsarr@users.noreply.github.com>
2021-09-04 21:14:17 +0530

    progress bar
    progress bar

---
+++ progressbar.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace progressbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i=0;i<=100;i++)
+            {
+                progressBar1.Value = i;
+                //Thread.Sleep(100); 
+                
+            }
+        }
+    }
+}
